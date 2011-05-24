d969f4abffc5b7efe14feb4a06272c61b3d6c2a5
Matthias Tretter <matthias.tretter@gmail.com>
2011-05-24 09:11:07 +0200

    Added empty project with PSFoundation

---
+++ Last.fm/Last/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  LastAppDelegate.h
+//  Last
+//
+//  Created by Matthias Tretter on 24.05.11.
+//  Copyright 2011 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Last.fm/Last/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Last
+//
+//  Created by Matthias Tretter on 24.05.11.
+//  Copyright 2011 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
